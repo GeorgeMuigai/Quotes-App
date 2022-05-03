ae27e9d7af641225c7d83a66897024f4b0ab9f9f
George Muigai <87810398+GeorgeMuigai@users.noreply.github.com>
2022-05-04 00:43:06 +0300

    Add files via upload

--- app/src/main/java/com/gdev/quotes/MainActivity.java
+++ app/src/main/java/com/gdev/quotes/MainActivity.java
@@ -5,8 +5,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +31,38 @@ public class MainActivity extends AppCompatActivity {
         rv_quotes.setLayoutManager(new LinearLayoutManager(this));
 
         // adding the adapter
+        adapter = new QuotesAdapter(quotesList);
+        rv_quotes.setAdapter(adapter);
+
+        retrofitClient();
+    }
+
+    private void retrofitClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://quotable.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        QuotesAPI quotesAPI = retrofit.create(QuotesAPI.class);
+
+        Call<List<QuotesModal>> quotes = quotesAPI.getQuotes();
+
+        quotes.enqueue(new Callback<List<QuotesModal>>() {
+            @Override
+            public void onResponse(Call<List<QuotesModal>> call, Response<List<QuotesModal>> response) {
+                if(!response.isSuccessful())
+                {
+                    Toast.makeText(MainActivity.this, "We encountered an Error when loading data", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                quotesList.addAll(response.body());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<QuotesModal>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
